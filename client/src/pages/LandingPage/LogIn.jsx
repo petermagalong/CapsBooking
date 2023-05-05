f93df42130676b299b0481ea0f37dac1c7feea31
Peter Christian Magalong <pmagalong@yondu.com>
2023-05-06 05:20:01 +0800

    implement integration login

--- client/src/pages/LandingPage/LogIn.jsx
+++ client/src/pages/LandingPage/LogIn.jsx
@@ -4,41 +4,33 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { useState } from "react";
+import { loginAuth } from "../../services/accounts";
 
 function LogIn() {
-  const initialValue = { email: "", password: "" }
+  const initialValue = { email_address: "", password: "" }
   const [formValues, setFormvalues] = useState(initialValue);
   const [formErrors, setFormErrors] = useState({})
   const [isSubmit, setSubmit] = useState()
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    console.log({ [name]: value }, "gggggg", value)
     setFormvalues({ ...formValues, [name]: value })
 
   }
-  const handlesSubmit = (e) => {
+  const handlesSubmit = async (e) => {
     e.preventDefault()
-    setFormErrors(validate(formValues));
-    setSubmit(true)
+   const { data , status } = await loginAuth(formValues)
+ 
+   if(status > '300'){
+    setFormErrors({errorVal:data.message})
+   }
+   if(status < '300'){
+    setFormErrors({})
+    localStorage.setItem("userId",data.status.userId)
+    localStorage.setItem("role",data.status.role)
+   }
+
   }
-  const validate = (values) => {
-    const errors = {};
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-    if (!values.email) {
-      errors.email = "Email is required!";
-    } else if (!regex.test(values.email)) {
-      errors.email = "This is not a valid email format!";
-    }
-    if (!values.password) {
-      errors.password = "Password is required";
-    } else if (values.password.length < 4) {
-      errors.password = "Password must be more than 4 characters";
-    } else if (values.password.length > 10) {
-      errors.password = "Password cannot exceed more than 10 characters";
-    }
-    return errors;
-  };
   return (
     <>
       <Card style={{ background: 'linear-gradient(90deg, rgba(69,11,7,1) 0%, rgba(7,25,71,1) 100%)', height: '88vh' }}>
@@ -52,11 +44,11 @@ function LogIn() {
               <Form onSubmit={handlesSubmit} style={{ padding: ' 80px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                 <Form.Group style={{ width: '100%' }} className="mb-3" controlId="formBasicEmail">
                   <Form.Label style={{ fontSize: 'x-large' }}>Email address</Form.Label>
-                  <Form.Control style={{ height: '50px' }} type="text" placeholder="Enter email" name='email' value={formValues.email || ""} onChange={handleChange} />
+                  <Form.Control style={{ height: '50px' }} type="text" placeholder="Enter email" name='email_address' value={formValues.email_address || ""} onChange={handleChange} />
                   <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
                   </Form.Text>
-                  <p>{formErrors.email}</p>
+                  <p>{formErrors.email_address}</p>
                 </Form.Group>
                 <Form.Group style={{ width: '100%' }} className="mb-3" controlId="formBasicEmail" >
                   <Form.Label style={{ fontSize: 'x-large' }}>Password</Form.Label>
@@ -69,6 +61,7 @@ function LogIn() {
                 <Button style={{ width: '450px', height: '50px', marginTop: '20px' }} variant="primary" type="submit">
                   Submit
                 </Button>
+                <p>{formErrors.errorVal}</p>
               </Form>
             </Card>
           </Col>
